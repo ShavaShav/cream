90f3ecb0a932aa83897dac8934ccc6e034db5022
ShavaShav <zachshaver@gmail.com>
2018-03-21 02:04:46 -0400

    Create initial project and navigation drawer
    Using a navigation drawer on main activity,
    where menu items change the main fragment
    
    Tested with overview and accounts fragment

---
+++ app/src/main/java/com/shaverz/cream/MainActivity.java
@@ -0,0 +1,84 @@
+package com.shaverz.cream;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, new OverviewFragment())
+                .commit();
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            // regular back press
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Start new fragment for selection
+        Fragment fragment = null;
+        boolean changeFragment = true;
+        switch (item.getItemId()) {
+            case R.id.nav_overview:
+                fragment = new OverviewFragment();
+                break;
+            case R.id.nav_accounts:
+                fragment = new AccountsFragment();
+                break;
+            case R.id.nav_transactions:
+                changeFragment = false;
+                break;
+            case R.id.nav_reports:
+                changeFragment = false;
+                break;
+            default:
+                changeFragment = false;
+        }
+
+        if (changeFragment) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
