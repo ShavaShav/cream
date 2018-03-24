19bafda3c99414c2dd2e886df3f39fc9467305f1
ShavaShav <zachshaver@gmail.com>
2018-03-24 05:12:18 -0400

    Add card layouts for overview and accounts views

--- app/src/main/java/com/shaverz/cream/AccountsFragment.java
+++ app/src/main/java/com/shaverz/cream/AccountsFragment.java
@@ -4,11 +4,15 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class AccountsFragment extends Fragment {
+
+    private View mView;
 
     public AccountsFragment() {
         // Required empty public constructor
@@ -17,8 +21,23 @@ public class AccountsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_accounts, container, false);
+        mView = inflater.inflate(R.layout.fragment_accounts, container, false);
+
+        // Attach settings menu to accounts card
+        Toolbar accountsToolbar = (Toolbar) mView.findViewById(R.id.toolbar_accounts);
+        if (accountsToolbar != null) {
+            accountsToolbar.inflateMenu(R.menu.menu_accounts);
+            accountsToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem menuItem) {
+                    return true;
+                }
+            });
+        }
+
+        return mView;
     }
 
     @Override
--- app/src/main/java/com/shaverz/cream/OverviewFragment.java
+++ app/src/main/java/com/shaverz/cream/OverviewFragment.java
@@ -4,11 +4,15 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class OverviewFragment extends Fragment {
+
+    private View mView;
 
     public OverviewFragment() {
         // Required empty public constructor
@@ -18,7 +22,48 @@ public class OverviewFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_overview, container, false);
+        mView = inflater.inflate(R.layout.fragment_overview, container, false);
+
+        // Attach settings menu to accounts card
+        Toolbar accountsToolbar = (Toolbar) mView.findViewById(R.id.toolbar_accounts);
+        if (accountsToolbar != null) {
+            accountsToolbar.inflateMenu(R.menu.menu_overview_accounts);
+            accountsToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem menuItem) {
+                    return true;
+                }
+            });
+        }
+
+        // Attach settings menu to transactions card
+        Toolbar transactionsBar = (Toolbar) mView.findViewById(R.id.toolbar_transactions);
+        if (transactionsBar != null) {
+            transactionsBar.inflateMenu(R.menu.menu_overview_transactions);
+            transactionsBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem menuItem) {
+                    return true;
+                }
+            });
+        }
+
+        // Attach settings menu to transactions card
+        Toolbar incomeExpenseBar = (Toolbar) mView.findViewById(R.id.toolbar_income_vs_expense);
+        if (incomeExpenseBar != null) {
+            incomeExpenseBar.inflateMenu(R.menu.menu_overview_income_vs_expenses);
+            incomeExpenseBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem menuItem) {
+                    switch (menuItem.getItemId()) {
+                        default:
+                            return true;
+                    }
+                }
+            });
+        }
+
+        return mView;
     }
 
     @Override
