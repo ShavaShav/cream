75cda924a3ede44fad3f15c100604ab52b1ae206
ShavaShav <zachshaver@gmail.com>
2018-04-08 03:58:26 -0400

    Implement reordering. Some bugs but works

--- app/src/main/java/com/shaverz/cream/views/OverviewCustomizationViewAdapter.java
+++ app/src/main/java/com/shaverz/cream/views/OverviewCustomizationViewAdapter.java
@@ -12,6 +12,7 @@ import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
+import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
 
@@ -33,6 +34,7 @@ public class OverviewCustomizationViewAdapter extends ArrayAdapter<String> {
     private String[] stringList;
     private List<Boolean> isVisibleList;
     private List<Integer> orderList;
+    private boolean reordering = false;
 
     public OverviewCustomizationViewAdapter(@NonNull Context context) {
         super(context, 0);
@@ -45,7 +47,7 @@ public class OverviewCustomizationViewAdapter extends ArrayAdapter<String> {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         // Get the data item for this position
         final int overviewItem = orderList.get(position);
         String overviewItemString = getItem(overviewItem);
@@ -56,27 +58,83 @@ public class OverviewCustomizationViewAdapter extends ArrayAdapter<String> {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.overview_customization_row, parent, false);
         }
         // Lookup view for data population
-        TextView itemTextView = (TextView) convertView.findViewById(R.id.row_text_view);
+        final ImageView upArrowView = (ImageView) convertView.findViewById(R.id.upArrow);
+        final ImageView downArrowView = (ImageView) convertView.findViewById(R.id.downArrow);
+
+        final TextView itemTextView = (TextView) convertView.findViewById(R.id.row_text_view);
         final Switch toggle = (Switch) convertView.findViewById(R.id.switch_id);
 
         // Populate the data into the template view using the data object
         itemTextView.setText(overviewItemString);
         toggle.setChecked(isVisible);
 
+        upArrowView.post(new Runnable() {
+            public void run() {
+                // update db entries whenever switch toggled
+                upArrowView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        // If reordering, don't allow movement
+                        if (!reordering) {
+                            reordering = true;
+
+                            if (position > 0) {
+                                // swap with item above in orderList
+                                int temp = orderList.get(position-1);
+                                orderList.set(position - 1, orderList.get(position));
+                                orderList.set(position, temp);
+
+                                saveOrderList();
+                                notifyDataSetChanged();
+                            }
+
+                            reordering = false;
+                        }
+                    }
+                });
+            }
+        });
+
+
+        downArrowView.post(new Runnable() {
+            public void run() {
+                // update db entries whenever switch toggled
+                downArrowView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        // If reordering, don't allow movement
+                        if (!reordering) {
+                            reordering = true;
+
+                            if (position < stringList.length - 1) {
+                                // swap with item below in orderList
+                                int temp = orderList.get(position+1);
+                                orderList.set(position + 1, orderList.get(position));
+                                orderList.set(position, temp);
+
+                                saveOrderList();
+                                notifyDataSetChanged();
+                            }
+
+                            reordering = false;
+                        }
+                    }
+                });
+            }
+        });
+
         // attach switch listener
         toggle.post(new Runnable() {
             public void run() {
+                // update db entries whenever switch toggled
                 toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
                     @Override
                     public void onCheckedChanged(CompoundButton compoundButton, boolean isVisible) {
-                        ContentValues content = new ContentValues();
-                        isVisibleList.set(overviewItem, isVisible);
-
-                        content.put(DB.User.COLUMN_OVERVIEW_VISIBILITY,
-                                OverviewCustomization.getVisibleDBString(isVisibleList));
-                        getContext().getContentResolver().update(
-                                DB.User.buildUserUri(Long.parseLong(MainActivity.CURRENT_USER.getId())),
-                                content, null, null);
+                        if (!reordering) {
+                            Log.d(Utils.TAG, "pos " + position + " visible: " + isVisible);
+                            isVisibleList.set(orderList.get(position), isVisible);
+                            saveVisibilityList();
+                        }
                     }
                 });
             }
@@ -84,5 +142,25 @@ public class OverviewCustomizationViewAdapter extends ArrayAdapter<String> {
 
         // Return the completed view to render on screen
         return convertView;
+    }
+
+    private void saveOrderList() {
+        ContentValues values = new ContentValues();
+        values.put(DB.User.COLUMN_OVERVIEW_ORDER,
+                OverviewCustomization.getOrderDBString(orderList));
+
+        getContext().getContentResolver().update(
+                DB.User.buildUserUri(Long.parseLong(MainActivity.CURRENT_USER.getId())),
+                values, null, null);
+    }
+
+    private void saveVisibilityList() {
+        ContentValues content = new ContentValues();
+
+        content.put(DB.User.COLUMN_OVERVIEW_VISIBILITY,
+                OverviewCustomization.getVisibleDBString(isVisibleList));
+        getContext().getContentResolver().update(
+                DB.User.buildUserUri(Long.parseLong(MainActivity.CURRENT_USER.getId())),
+                content, null, null);
     }
 }
